aee7b7d219be30e07d21fadab4d03315a657d334
Niket714 <niketralebhat2004@gmail.com>
2025-03-20 20:49:57 +0530

    first commit

---
+++ backend/controllers/Faculty/credential.controller.js
@@ -0,0 +1,127 @@
+const facultyCredential = require("../../models/Faculty/credential.model.js");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const loginHandler = async (req, res) => {
+  let { loginid, password } = req.body;
+  try {
+    let user = await facultyCredential.findOne({ loginid });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Wrong Credentials" });
+    }
+
+    const staticSalt = process.env.STATIC_SALT || "mySecretStaticSalt";
+    const isMatch = await bcrypt.compare(
+      user.dynamic_salt + password + staticSalt,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Wrong Credentials" });
+    }
+
+    const data = {
+      success: true,
+      message: "Login Successfull!",
+      loginid: user.loginid,
+      id: user.id,
+    };
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const registerHandler = async (req, res) => {
+  let { loginid, password } = req.body;
+  try {
+    let user = await facultyCredential.findOne({ loginid });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User With This LoginId Already Exists",
+      });
+    }
+
+    const staticSalt = process.env.STATIC_SALT || "mySecretStaticSalt";
+    const dynamicSalt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(
+      dynamicSalt + password + staticSalt,
+      10
+    );
+
+    user = await facultyCredential.create({
+      loginid,
+      password: hashedPassword,
+      dynamic_salt: dynamicSalt,
+    });
+
+    const data = {
+      success: true,
+      message: "Register Successfull!",
+      loginid: user.loginid,
+      id: user.id,
+    };
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const updateHandler = async (req, res) => {
+  try {
+    let user = await facultyCredential.findById(req.params.id);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "No User Exists!",
+      });
+    }
+     // If updating password, hash the new password
+    if (req.body.password) {
+        const staticSalt = process.env.STATIC_SALT || "mySecretStaticSalt";
+        const dynamicSalt = await bcrypt.genSalt(10);
+        req.body.dynamic_salt = dynamicSalt;
+        req.body.password = await bcrypt.hash(dynamicSalt + req.body.password + staticSalt, 10);
+    }
+
+    await facultyCredential.findByIdAndUpdate(req.params.id, req.body);
+    
+    const data = {
+      success: true,
+      message: "Updated Successfull!",
+    };
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const deleteHandler = async (req, res) => {
+  try {
+    let user = await facultyCredential.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "No User Exists!",
+      });
+    }
+    const data = {
+      success: true,
+      message: "Deleted Successfull!",
+    };
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports = { loginHandler, registerHandler, updateHandler,deleteHandler};
---
+++ backend/controllers/Student/credential.controller.js
@@ -0,0 +1,124 @@
+const studentCredential = require("../../models/Students/credential.model.js");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const loginHandler = async (req, res) => {
+    let { loginid, password } = req.body;
+    try {
+        let user = await studentCredential.findOne({ loginid });
+        if (!user) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Wrong Credentials" });
+        }
+        
+        const staticSalt = process.env.STATIC_SALT || "mySecretStaticSalt";
+        const isMatch = await bcrypt.compare(user.dynamic_salt + password + staticSalt, user.password);
+
+        if (!isMatch) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Wrong Credentials" });
+        }
+        
+        
+        const data = {
+            success: true,
+            message: "Login Successfull!",
+            loginid: user.loginid,
+            id: user.id,
+        };
+        res.json(data);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+const registerHandler = async (req, res) => {
+    let { loginid, password } = req.body;
+    try {
+        let user = await studentCredential.findOne(req.body);
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: "User With This LoginId Already Exists",
+            });
+        }
+
+        const staticSalt = process.env.STATIC_SALT || "mySecretStaticSalt"; 
+        const dynamicSalt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(dynamicSalt + password + staticSalt, 10);
+
+        user = await studentCredential.create({
+            loginid,
+            password: hashedPassword,
+            dynamic_salt: dynamicSalt,
+        });
+
+        const data = {
+            success: true,
+            message: "Register Successfull!",
+            loginid: user.loginid,
+            id: user.id,
+        };
+        res.json(data);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+const updateHandler = async (req, res) => {
+    try {
+        let user = await studentCredential.findById(
+            req.params.id
+        );
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "No User Exists!",
+            });
+        }
+        // If updating password, hash the new password
+        if (req.body.password) {
+            const staticSalt = process.env.STATIC_SALT || "mySecretStaticSalt";
+            const dynamicSalt = await bcrypt.genSalt(10);
+            req.body.dynamic_salt = dynamicSalt;
+            req.body.password = await bcrypt.hash(dynamicSalt + req.body.password + staticSalt, 10);
+        }
+
+        await studentCredential.findByIdAndUpdate(req.params.id, req.body);
+        
+        const data = {
+            success: true,
+            message: "Updated Successfull!",
+        };
+        res.json(data);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+const deleteHandler = async (req, res) => {
+    try {
+        let user = await studentCredential.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "No User Exists!",
+            });
+        }
+        const data = {
+            success: true,
+            message: "Deleted Successfull!",
+        };
+        res.json(data);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+module.exports = { loginHandler, registerHandler, updateHandler, deleteHandler }
