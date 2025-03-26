a945d92e8cb5eebbb762969cb02745182a63a228
Niket714 <niketralebhat2004@gmail.com>
2025-03-26 14:59:56 +0530

    curriculum update

--- frontend/src/Screens/Faculty/Home.jsx
+++ frontend/src/Screens/Faculty/Home.jsx
@@ -6,6 +6,7 @@ import Profile from "./Profile";
 import Timetable from "./Timetable";
 import { Toaster } from "react-hot-toast";
 import Material from "./Material";
+import Curriculum from "./Curriculum";
 import Marks from "./Marks";
 import Student from "./Student";
 const Home = () => {
@@ -79,6 +80,16 @@ const Home = () => {
               </li>
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                  selectedMenu === "Curriculum"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+                }`}
+                onClick={() => setSelectedMenu("Curriculum")}
+              >
+                Curriculum
+              </li>
+              <li
+                className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Material"
                     ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
@@ -93,6 +104,7 @@ const Home = () => {
             {selectedMenu === "Material" && <Material />}
             {selectedMenu === "Notice" && <Notice />}
             {selectedMenu === "My Profile" && <Profile />}
+            {selectedMenu === "Curriculum" && <Curriculum />}
             {selectedMenu === "Student Info" && <Student />}
           </div>
         </>
--- frontend/src/Screens/Student/Home.jsx
+++ frontend/src/Screens/Student/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import Profile from "./Profile";
 import Timetable from "./Timetable";
+import Curriculum from "./Curriculum";
 import Marks from "./Marks";
 import Notice from "../../components/Notice";
 import Material from "./Material";
@@ -67,6 +68,16 @@ const Home = () => {
               </li>
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                  selectedMenu === "Curriculum"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+                }`}
+                onClick={() => setSelectedMenu("Curriculum")}
+              >
+                Curriculum
+              </li>
+              <li
+                className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Notice"
                     ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
@@ -80,6 +91,7 @@ const Home = () => {
             {selectedMenu === "Marks" && <Marks />}
             {selectedMenu === "Material" && <Material />}
             {selectedMenu === "Notice" && <Notice />}
+            {selectedMenu === "Curriculum" && <Curriculum />}
             {selectedMenu === "My Profile" && <Profile />}
           </div>
         </>
